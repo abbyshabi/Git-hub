6bd438201b4de15d9913ba0bae6d7eda8f044810
abby-shabi <abbyshabi@gmail.com>
2019-01-21 20:43:53 +0300

    imported files into my profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService} from '../profile.service';
+import { Profile} from '../profile'
 
 @Component({
   selector: 'app-profile',
