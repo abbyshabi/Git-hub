9aa75d99f2384d30dedee6382cf948a9748a2741
abby-shabi <abbyshabi@gmail.com>
2019-01-21 20:45:25 +0300

    added the get profile function

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,7 +11,16 @@ export class ProfileComponent implements OnInit {
   private profile: any[];
   private repos :any[]; 
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService) { 
+    this.profileService.getProfileInfo().subscribe(profile=>{
+      console.log(profile);
+      this.profile= profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos =>{
+      console.log(repos);
+      this.repos = repos;
+    })
+  }
 
   ngOnInit() {
   }
