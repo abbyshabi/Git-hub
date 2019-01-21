965d81b1a574d575efe193089224056e59472cf4
abby-shabi <abbyshabi@gmail.com>
2019-01-21 20:29:48 +0300

    updated the app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,39 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
 import{ FormsModule} from '@angular/forms';
-
+import { RepoComponent } from './repo/repo.component';
 import {RouterModule} from '@angular/router';
-
+import { HighlightDirective } from './highlight.directive';
+import { ShowComponent } from './show/show.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RepoComponent,
+    HighlightDirective,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+     path:'repo',
+     component:RepoComponent
+    },
+    {
+      path:'',
+      component:ProfileComponent
+     }
+    ])
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
