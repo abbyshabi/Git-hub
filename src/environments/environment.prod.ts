d7fdc2ae5cc68eec0e62795afb13a0416c18238b
abby-shabi <abbyshabi@gmail.com>
2019-01-21 20:20:11 +0300

    added the access key to the enviroment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/abbyshabi?access_token=d2c75a4d9ecfb3fc0c778d5821f034f7a6b1717e',
+  
 };
