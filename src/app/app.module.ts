e5422095a33c2087bd35323f7134cc398e1278c3
abby-shabi <abbyshabi@gmail.com>
2019-01-21 20:33:11 +0300

    created the profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+
 import { ProfileService } from './profile.service';
 import{ FormsModule} from '@angular/forms';
-import { RepoComponent } from './repo/repo.component';
+
 import {RouterModule} from '@angular/router';
-import { HighlightDirective } from './highlight.directive';
-import { ShowComponent } from './show/show.component'
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    RepoComponent,
-    HighlightDirective,
-    ShowComponent
+   
   ],
   imports: [
     BrowserModule,
